7defb39e25ac66dca0b478488fb878b992e1d060
Pushpavel <jpushpavel@gmail.com>
2021-06-27 16:13:29 +0530

    🚧: SelectedStandCard with StreamBuilder

--- lib/widgets/SelectedStandCard.dart
+++ lib/widgets/SelectedStandCard.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:umbrella_client/models/Stand.dart';
+import 'package:umbrella_client/services/StandService.dart';
 
 import 'StandCard.dart';
 
 class SelectedStandCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var standService = Provider.of<StandService>(context);
+
     return Card(
       child: InkWell(
         child: Padding(
@@ -13,7 +17,13 @@ class SelectedStandCard extends StatelessWidget {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              StandContent(Stand("Nitpy Boys Hostel")),
+              StreamBuilder<Stand>(
+                  stream: standService.getSelectedStand(),
+                  builder: (context, snapshot) {
+                    if (snapshot.data == null)
+                      return CircularProgressIndicator();
+                    return StandContent(snapshot.data!);
+                  }),
               Icon(Icons.unfold_more),
             ],
           ),
