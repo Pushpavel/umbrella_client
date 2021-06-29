964ae99b29e9658788a623e3de4d8fcb0e11053d
Pushpavel <jpushpavel@gmail.com>
2021-06-29 09:14:56 +0530

    💩: added SplashScreen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:umbrella_client/pages/SplashScreen.dart';
 import 'package:umbrella_client/resources/AppThemeData.dart';
-import 'package:umbrella_client/resources/Routes.dart';
+import 'package:umbrella_client/resources/Providers.dart';
 
-void main() => runApp(MaterialApp(
-    theme: appThemeData,
-    initialRoute: '/',
-    routes: {'/': Routes.pickup},
-  ));
+void main() => runApp(MultiProvider(
+      providers: [Providers.authService(), Providers.standService()],
+      child: MaterialApp(
+        theme: appThemeData,
+        home: SplashScreen(onInit),
+      ),
+    ));
+
+void onInit(BuildContext context) {
+  // TODO: redirect to appropriate screen
+}
--- lib/resources/Providers.dart
+++ lib/resources/Providers.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
+import 'package:umbrella_client/services/AuthService.dart';
+import 'package:umbrella_client/services/AuthServiceImpl.dart';
 import 'package:umbrella_client/services/StandService.dart';
 import 'package:umbrella_client/services/StandServiceImpl.dart';
 
-class Providers {
+abstract class Providers {
   static standService({Widget? child}) => Provider<StandService>(
-      create: (_) => StandServiceImpl(),
-      dispose: (_, service) => service.dispose(),
-      child: child,
-    );
+        create: (_) => StandServiceImpl(),
+        dispose: (_, service) => service.dispose(),
+        child: child,
+      );
+
+  static authService({Widget? child}) => Provider<AuthService>(
+        create: (_) => AuthServiceImpl(),
+        child: child,
+      );
 }
