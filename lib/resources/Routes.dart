1c2dab2432e45fce98be8fcb45ef0294b9af2d27
Pushpavel <jpushpavel@gmail.com>
2021-06-29 09:05:52 +0530

    💩: added LoginScreen.dart

--- lib/resources/Routes.dart
+++ lib/resources/Routes.dart
@@ -2,11 +2,15 @@ import 'dart:collection';
 
 import 'package:umbrella_client/models/Stand.dart';
 import 'package:umbrella_client/pages/DropScreen.dart';
+import 'package:umbrella_client/pages/LoginScreen.dart';
 import 'package:umbrella_client/pages/PickupScreen.dart';
 import 'package:umbrella_client/pages/StandListScreen.dart';
 import 'package:umbrella_client/resources/Providers.dart';
 
 class Routes {
+  // LoginScreen
+  static final login = (_) => LoginScreen();
+
   // PickupScreen
   static final pickup = (_) => PickupScreen();
 
