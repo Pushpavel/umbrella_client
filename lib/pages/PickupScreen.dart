cc9e5b5a66e954927bf0dfe8b5f9b69489f2f87a
Pushpavel <jpushpavel@gmail.com>
2021-06-29 13:20:12 +0530

    🍧: updated PickupScreen.dart UI

--- lib/pages/PickupScreen.dart
+++ lib/pages/PickupScreen.dart
@@ -1,88 +1,26 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:umbrella_client/widgets/SelectedStandCard.dart';
 
-/// This is the stateful widget that the main application instantiates.
-class PickupScreen extends StatefulWidget {
-  const PickupScreen({Key? key}) : super(key: key);
-
-  @override
-  State<PickupScreen> createState() => _MyStatefulWidgetState();
-}
-
-/// This is the private State class that goes with MyStatefulWidget.
-class _MyStatefulWidgetState extends State<PickupScreen> {
-  String dropdownValue = 'One';
-  Widget buildDropDownButton(){
-    return Container(
-      alignment: Alignment.center,
-      color: Colors.lightBlue,
-      child:  DropdownButton<String>(
-        value: dropdownValue,
-        icon: const Icon(Icons.arrow_drop_down_circle),
-        iconSize: 24,
-        elevation: 16,
-        style: const TextStyle(color: Colors.deepPurple),
-        onChanged: (String? newValue) {
-          setState(() {
-            dropdownValue = newValue!;
-          });
-
-        },
-        items: <String>['One', 'Two', 'Three', 'Four']
-            .map<DropdownMenuItem<String>>((String value) {
-          return DropdownMenuItem<String>(
-            value: value,
-            child: Text(value),
-
-          );
-        }).toList(),
-      ),
-    );
-  }
+class PickupScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        alignment: Alignment.center,
-        color: Colors.lightBlue,
-        child:  DropdownButton<String>(
-          value: dropdownValue,
-          icon: const Icon(Icons.arrow_drop_down_circle),
-          iconSize: 24,
-          elevation: 16,
-          style: const TextStyle(color: Colors.black),
-          onChanged: (String? newValue) {
-            setState(() {
-              dropdownValue = newValue!;
-            });
-
-          },
-          items: <String>['One', 'Two', 'Three', 'Four']
-              .map<DropdownMenuItem<String>>((String value) {
-            return DropdownMenuItem<String>(
-              value: value,
-              child: Text(value),
-
-            );
-          }).toList(),
-        ),
-      ),
-      backgroundColor: Color(0xffff40ff),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.navigation),
-        backgroundColor: Colors.green,
-        onPressed: () =>showDialog<String>(
-          context: context,
-          builder: (BuildContext context) => AlertDialog(
-            title: const Text('Umbrella'),
-            content: const Text('Request Granted'),
-          ),
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SelectedStandCard(),
+          Container(
+            padding: EdgeInsets.all(32),
+            alignment: Alignment.bottomCenter,
+            child: FloatingActionButton.extended(
+              icon: Icon(Icons.umbrella),
+              label: Text("REQUEST UMBRELLA"),
+              onPressed: () {},
+            ),
+          )
+        ],
       ),
     );
-
-
   }
-
 }
-
