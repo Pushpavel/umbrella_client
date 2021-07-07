40e5915361374d2a46c696c0b0d370e7c9c683b0
Pushpavel <jpushpavel@gmail.com>
2021-07-07 15:10:39 +0530

    🔥: removed UmbrellaService.dart

--- lib/resources/Providers.dart
+++ lib/resources/Providers.dart
@@ -2,18 +2,10 @@ import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 import 'package:umbrella_client/services/StandService.dart';
 import 'package:umbrella_client/services/StandServiceImpl.dart';
-import 'package:umbrella_client/services/UmbrellaService.dart';
-import 'package:umbrella_client/services/UmbrellaServiceImpl.dart';
 
 abstract class Providers {
   static standService({Widget? child}) => Provider<StandService>(
         create: (_) => StandServiceImpl(),
-        dispose: (_, service) => service.dispose(),
-        child: child,
-      );
-
-  static umbrellaService({Widget? child}) => Provider<UmbrellaService>(
-        create: (_) => UmbrellaServiceImpl(),
         dispose: (_, service) => service.dispose(),
         child: child,
       );
