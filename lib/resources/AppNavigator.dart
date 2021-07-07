babfa9b4d44ca70889179aa4fcdfe26b194ec00c
Pushpavel <jpushpavel@gmail.com>
2021-07-07 15:21:39 +0530

    🚧: initial page logic accounted for user's current request

--- lib/resources/AppNavigator.dart
+++ lib/resources/AppNavigator.dart
@@ -2,11 +2,16 @@ import 'dart:async';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:umbrella_client/models/UmbrellaRequest.dart';
+import 'package:umbrella_client/pages/DropScreen.dart';
 import 'package:umbrella_client/pages/HomeScreen.dart';
 import 'package:umbrella_client/pages/LoginScreen.dart';
+import 'package:umbrella_client/pages/PickupScreen.dart';
 import 'package:umbrella_client/pages/SplashScreen.dart';
+import 'package:umbrella_client/repositories/UmbrellaRepo.dart';
 import 'package:umbrella_client/services/AuthService.dart';
 import 'package:umbrella_client/utils/nav-utils.dart';
+import 'package:rxdart/rxdart.dart';
 
 class AppNavigator extends StatefulWidget {
   final AuthService authService;
@@ -27,7 +32,7 @@ class _AppNavigatorState extends State<AppNavigator> {
   void initState() {
     super.initState();
 
-    _sub = authService.user.listen((user) async {
+    _sub = authService.user.switchMap((user) async* {
       // unauthenticated user
       if (user == null) {
         await context.resetNavStackWith(LoginScreen());
@@ -40,8 +45,17 @@ class _AppNavigatorState extends State<AppNavigator> {
         return;
       }
 
-      // TODO: user with request
-    });
+      final requestStream = UmbrellaRepo.getUmbrellaRequest(user.requestId!);
+
+      yield* requestStream.switchMap((request) async* {
+        if (request == null) throw Exception("Internal Error");
+
+        if (request.status == UmbrellaRequestStatus.REQUESTED)
+          await context.resetNavStackWith(PickupScreen());
+        else
+          await context.resetNavStackWith(DropScreen());
+      });
+    }).listen((event) => null);
   }
 
   @override
