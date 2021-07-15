7a00d71f6f6fb29a209447dac13733b0935d66f1
Pushpavel <jpushpavel@gmail.com>
2021-07-15 16:14:22 +0530

    ✨: disable request button if stand is busy

--- lib/data/models/Stand.dart
+++ lib/data/models/Stand.dart
@@ -3,15 +3,20 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class Stand {
   final String name;
   final String id;
+  final String requestId;
 
-  Stand(this.name, this.id);
-
-  factory Stand.fromDynamic(String key, data) {
-    return Stand(data["name"], key);
-  }
+  Stand({
+    required this.id,
+    required this.name,
+    required this.requestId,
+  });
 
   factory Stand.fromFirestore(DocumentSnapshot<Map<String, dynamic>> snap) {
-    final data  = snap.data();
-    return Stand(data?["name"], snap.id);
+    final data = snap.data();
+    return Stand(
+      id: snap.id,
+      name: data?["name"],
+      requestId: data?["requestId"],
+    );
   }
 }
--- lib/pages/HomeScreen.dart
+++ lib/pages/HomeScreen.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:umbrella_client/data/models/Stand.dart';
 import 'package:umbrella_client/data/repositories/UmbrellaRepo.dart';
 import 'package:umbrella_client/data/services/HomeScreenViewModel.dart';
 import 'package:umbrella_client/data/services/StandService.dart';
 import 'package:umbrella_client/helpers/DisposableProvider.dart';
 import 'package:umbrella_client/helpers/extensions/ContextExtensions.dart';
 import 'package:umbrella_client/widgets/SelectedStandCard.dart';
+import 'package:rxdart/rxdart.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -24,6 +26,7 @@ class _HomeScreenView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final model = context.get<HomeScreenViewModel>();
+    final standService = context.get<StandService>();
 
     return Scaffold(
       body: Column(
@@ -36,15 +39,25 @@ class _HomeScreenView extends StatelessWidget {
           Container(
             padding: EdgeInsets.all(32),
             alignment: Alignment.bottomCenter,
-            child: FloatingActionButton.extended(
-              icon: Icon(Icons.umbrella),
-              label: Text("REQUEST UMBRELLA"),
-              onPressed: () async {
-                final selectedStandId = model.selectedStandId$.value;
-                await UmbrellaRepo.requestUmbrellaPickup(selectedStandId);
-                // TODO: handle fail state
-              },
-            ),
+            child: StreamBuilder<Stand?>(
+                stream:
+                    model.selectedStandId$.switchMap((standId) => standService.getStand(standId)),
+                builder: (context, snapshot) {
+                  final enabled = snapshot.data?.requestId == null;
+                  print(enabled);
+
+                  return FloatingActionButton.extended(
+                    icon: Icon(Icons.umbrella),
+                    label: Text("REQUEST UMBRELLA"),
+                    onPressed: enabled
+                        ? () async {
+                            final selectedStandId = model.selectedStandId$.value;
+                            await UmbrellaRepo.requestUmbrellaPickup(selectedStandId);
+                            // TODO: handle fail state
+                          }
+                        : null,
+                  );
+                }),
           )
         ],
       ),
