b787074b55b9d42e56c91c32eba3066a8c3ff8ff
pushpavel <jpushpavel@gmail.com>
2021-07-30 11:17:25 +0530

    🚧: updated Stand.dart model and added status enum

--- lib/data/models/Stand.dart
+++ lib/data/models/Stand.dart
@@ -4,19 +4,37 @@ class Stand {
   final String name;
   final String id;
   final String? requestId;
+  final bool online;
+  final int umbrellaCount;
+  final int capacity;
 
   Stand({
     required this.id,
     required this.name,
+    required this.online,
+    required this.umbrellaCount,
+    required this.capacity,
     this.requestId,
   });
 
+  StandStatus getStatus() {
+    if (!online) return StandStatus.OFFLINE;
+    if (requestId != null) return StandStatus.BUSY;
+    if (umbrellaCount <= 0) return StandStatus.EMPTY;
+    return StandStatus.READY;
+  }
+
   factory Stand.fromFirestore(DocumentSnapshot<Map<String, dynamic>> snap) {
-    final data = snap.data();
+    final data = snap.data()!;
     return Stand(
       id: snap.id,
-      name: data?["name"],
-      requestId: data?["requestId"],
+      name: data["name"],
+      requestId: data["requestId"],
+      online: data["online"],
+      umbrellaCount: data["umbrellaCount"],
+      capacity: data["capacity"],
     );
   }
 }
+
+enum StandStatus { BUSY, EMPTY, OFFLINE, READY }
