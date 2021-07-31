85e837d2128f3042c590b7bfd0421706eed74cab
pushpavel <jpushpavel@gmail.com>
2021-07-31 11:51:48 +0530

    🚚: reorganized AppThemeData.dart

---
+++ lib/resources/theme/colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xff048fff);
+  static const primaryVariant = Color(0xffeff7ff);
+  static const secondary = Color.fromRGBO(85, 85, 85, 1);
+  static const secondaryVariant = Color.fromRGBO(255, 184, 0, 1);
+  static const error = Color.fromRGBO(255, 94, 4, 1);
+
+  static const whiteHigh = Colors.white;
+  static const whiteMedium = Color(0xbcffffff); // white74
+  static const whiteDisabled = Colors.white38;
+
+  static const blackHigh = Colors.black87;
+  static const blackMedium = Color(0xbcffffff); // black60
+  static const blackDisabled = Colors.black38;
+}
