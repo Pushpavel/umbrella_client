65ccbb07f6544478b1f51db6734a085d8e000be2
pushpavel <jpushpavel@gmail.com>
2021-07-31 21:44:30 +0530

    🚧: added requestProvider

--- lib/data/providers/root.dart
+++ lib/data/providers/root.dart
@@ -1,12 +1,24 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:umbrella_client/data/models/UmbrellaRequest.dart';
 import 'package:umbrella_client/data/models/UmbrellaRequest_.dart';
 import 'package:umbrella_client/data/repositories/AuthRepo.dart';
+import 'package:umbrella_client/data/repositories/RequestRepo.dart';
 import 'package:umbrella_client/data/repositories/UmbrellaRepo.dart';
 import 'package:umbrella_client/helpers/extensions/providerExtensions.dart';
 import 'package:umbrella_client/utils/lang-utils.dart';
 
 final authProvider = StreamProvider((_) => AuthRepo.getUser());
 
+final currentRequestIdProvider = StreamProvider<String?>((ref) {
+  return ref.read(authProvider.stream).map((user) => user?.requestId);
+});
+
+final requestProvider = StreamProvider.family<UmbrellaRequest?, String>((ref, requestId) {
+  return RequestRepo.getUmbrellaRequest(requestId);
+});
+
+@Deprecated("use currentRequestProvider")
 final currentUmbrellaRequestProvider = StreamProvider<UmbrellaRequest_?>((ref) {
   final userResult = ref.watch(authProvider).asResult();
 
