53449c13fa8e7ffbafe5a091932155043a963b8b
pushpavel <jpushpavel@gmail.com>
2021-08-01 16:59:43 +0530

    🚧: replaced main.dart with updated Screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:umbrella_client/helpers/simpleNavigator.dart';
-import 'package:umbrella_client/nav.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:umbrella_client/data/providers/root.dart';
+import 'package:umbrella_client/helpers/extensions/providerExtensions.dart';
+import 'package:umbrella_client/pages/ErrorScreen.dart';
+import 'package:umbrella_client/pages/HomeScreen.dart';
+import 'package:umbrella_client/pages/auth/LoginScreen.dart';
 import 'package:umbrella_client/resources/theme/AppThemeData.dart';
 
 void main() async {
@@ -12,11 +17,34 @@ void main() async {
 
   runApp(
     ProviderScope(
-      child: MaterialApp.router(
+      child: MaterialApp(
         theme: appThemeData,
-        routerDelegate: NavDelegate(),
-        routeInformationParser: DumbRouteInfoParser(),
+        home: _App(),
       ),
     ),
   );
+}
+
+class _App extends HookConsumerWidget {
+  const _App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(context, ref) {
+    final userResult = ref.watch(authProvider).asResult();
+
+    return userResult.when(
+          (user) {
+        if (user != null)
+          return HomeScreen();
+        else
+          return LoginScreen();
+      },
+      error: (err) => ErrorScreen(err: err),
+      loading: () => Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      ),
+    );
+  }
 }
