2d38f49b671f4fa4f38da529130aa6b28b0a5c52
pushpavel <jpushpavel@gmail.com>
2021-08-01 17:03:32 +0530

    🚧: added AppScreens.dart with static functions to build MaterialPageRoute for navigatable screens

---
+++ lib/resources/AppScreens.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:umbrella_client/pages/HomeScreen.dart';
+import 'package:umbrella_client/pages/LoginScreen.dart';
+import 'package:umbrella_client/pages/selectStand/SelectStandScreen.dart';
+
+class AppScreens {
+  AppScreens._();
+
+  static MaterialPageRoute homeScreen() => MaterialPageRoute(builder: (_) => HomeScreen());
+
+  static MaterialPageRoute loginScreen() => MaterialPageRoute(builder: (_) => LoginScreen());
+
+  static MaterialPageRoute selectStandScreen() => MaterialPageRoute(builder: (_) => SelectStandScreen());
+}
