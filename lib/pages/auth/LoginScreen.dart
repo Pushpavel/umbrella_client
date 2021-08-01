a6c0f9b2fa8bb3336d13a3badc7703555daa672d
pushpavel <jpushpavel@gmail.com>
2021-08-01 12:49:24 +0530

    🚚: extracted GoogleSignInButton into separate file

--- lib/pages/auth/LoginScreen.dart
+++ lib/pages/auth/LoginScreen.dart
@@ -1,14 +1,13 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:umbrella_client/data/models/UmbrellaUser.dart';
 import 'package:umbrella_client/data/providers/root.dart';
 import 'package:umbrella_client/data/repositories/AuthRepo.dart';
 import 'package:umbrella_client/helpers/extensions/providerExtensions.dart';
 import 'package:umbrella_client/helpers/result/Result.dart';
-import 'package:umbrella_client/resources/icons/AppIcons.dart';
-import 'package:umbrella_client/resources/theme/colors.dart';
+
+import 'GoogleSignInButton.dart';
 
 class LoginScreen extends HookConsumerWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -61,49 +60,3 @@ class LoginScreen extends HookConsumerWidget {
   }
 }
 
-class GoogleSignInButton extends StatelessWidget {
-  final VoidCallback? onPressed;
-
-  const GoogleSignInButton({
-    Key? key,
-    required this.onPressed,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final borderRadius = BorderRadius.all(Radius.circular(2));
-
-    return Card(
-      color: AppColors.googleBlue,
-      shape: RoundedRectangleBorder(borderRadius: borderRadius),
-      clipBehavior: Clip.antiAlias,
-      child: InkWell(
-        onTap: onPressed,
-        splashFactory: InkRipple.splashFactory,
-        child: Wrap(
-          crossAxisAlignment: WrapCrossAlignment.center,
-          spacing: 8,
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: borderRadius,
-                color: Colors.white,
-              ),
-              margin: EdgeInsets.all(2),
-              padding: EdgeInsets.all(8),
-              child: SvgPicture.asset(
-                AppIcons.googleLogo,
-                height: 24,
-                width: 24,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 12),
-              child: Text("Sign in with Google", style: Theme.of(context).primaryTextTheme.headline3),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
