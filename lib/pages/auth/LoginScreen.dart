1d1c575f8cf41c96e3d77e573e5fd58b9a49bf08
pushpavel <jpushpavel@gmail.com>
2021-08-01 12:47:33 +0530

    🚧: added signIn call in LoginScreen.dart

--- lib/pages/auth/LoginScreen.dart
+++ lib/pages/auth/LoginScreen.dart
@@ -1,13 +1,46 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:umbrella_client/data/models/UmbrellaUser.dart';
+import 'package:umbrella_client/data/providers/root.dart';
+import 'package:umbrella_client/data/repositories/AuthRepo.dart';
+import 'package:umbrella_client/helpers/extensions/providerExtensions.dart';
+import 'package:umbrella_client/helpers/result/Result.dart';
 import 'package:umbrella_client/resources/icons/AppIcons.dart';
 import 'package:umbrella_client/resources/theme/colors.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends HookConsumerWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(context, ref) {
+    final userResult = ref.watch(authProvider).asResult();
+
+    Widget content;
+    if (userResult is Success<UmbrellaUser?>) {
+      if (userResult.value == null)
+        content = GoogleSignInButton(onPressed: () async {
+          try {
+            // TODO: start loading
+            await AuthRepo.signInWithGoogle();
+          } catch (e) {
+            if (e is FirebaseAuthException) {
+              // TODO: toast signIn error
+            } else {
+              // TODO: toast Internal Error
+            }
+          } finally {
+            // TODO: stop loading
+          }
+        });
+      else {
+        // TODO: navigate to splash screen and toast
+        content = Text("Successfully SignedIn as ${userResult.value!.name}");
+      }
+    } else
+      content = CircularProgressIndicator();
+
     return Scaffold(
       body: SafeArea(
         child: Center(
@@ -19,9 +52,7 @@ class LoginScreen extends StatelessWidget {
               SizedBox(height: 32),
               Text("Umbrella Login", style: Theme.of(context).textTheme.headline1),
               SizedBox(height: 72),
-              GoogleSignInButton(
-                onPressed: () {},
-              )
+              content,
             ],
           ),
         ),
