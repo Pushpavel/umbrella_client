53312d4ee74a5015aea0a259d7d76cfb53223103
pushpavel <jpushpavel@gmail.com>
2021-08-01 22:04:08 +0530

    🚧: added ProfileScreen to AppScreens.dart

--- lib/resources/AppScreens.dart
+++ lib/resources/AppScreens.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:umbrella_client/pages/HomeScreen.dart';
 import 'package:umbrella_client/pages/auth/LoginScreen.dart';
+import 'package:umbrella_client/pages/auth/ProfileScreen.dart';
 import 'package:umbrella_client/pages/selectStand/SelectStandScreen.dart';
 
 class AppScreens {
@@ -11,4 +12,6 @@ class AppScreens {
   static MaterialPageRoute loginScreen() => MaterialPageRoute(builder: (_) => LoginScreen());
 
   static MaterialPageRoute selectStandScreen() => MaterialPageRoute(builder: (_) => SelectStandScreen());
+
+  static MaterialPageRoute profileScreen() => MaterialPageRoute(builder: (_) => ProfileScreen());
 }
